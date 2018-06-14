41a6912caee2051c9422d8325fcb2ee9a84f3fed
ivaniciojr <jsiilva@outlook.com.br>
2018-06-14 16:06:00 -0300

    Implementada a função de desempilhar uma pilha e reverter a ordem dos caracteres

--- src/br/com/ivaniciojr/pilhas/EmpilhaCaracteres.java
+++ src/br/com/ivaniciojr/pilhas/EmpilhaCaracteres.java
@@ -22,23 +22,55 @@ public class EmpilhaCaracteres {
             char[] arranjoCaracteres = String.valueOf(entrada).toCharArray();
 
             for (char caractere: arranjoCaracteres) {
+
                 if (caractere != ' ') {
-                    if (verificaCaractere(caractere)) {
-                        // numero
-                        System.out.println("LETRA: " + caractere);
-                    } else {
-                        // letra
-                        System.out.println("NUMERO: " + caractere);
-                    }
+                    if (verificaCaractere(caractere))
+                        // É um número
+                        pilhaCaracteres.push(caractere);
+                    else
+                        // Caso contrário, é uma letra
+                        pilhaNumeros.push(caractere);
+
                 }
             }
 
         } else System.out.println("ERRO! Digite algum arranjo de letras e/ou números.");
 
+        /*
+        *
+        * Desempilha a pilha de caracteres
+        *
+        * */
+        desempilha(pilhaCaracteres);
+
+        /*
+         *
+         * Desempilha a pilha de números
+         *
+         * */
+        desempilha(pilhaNumeros);
     }
 
     public static boolean verificaCaractere (char c) {
 
         return !Character.isDigit(c);
+    }
+
+    public static void desempilha (Stack<Character> pilha) {
+
+        int tamanhoPilha = pilha.size();
+        char[] auxiliar = new char[tamanhoPilha];
+
+        // Desempilha e coloca cada caractere da pilha em um vetor auxiliar
+        for (int i = 0; i < tamanhoPilha; i++) {
+            auxiliar[i] = pilha.pop();
+        }
+
+        // Faz um loop reverso e coloca todos os caracteres na sua devida ordem
+        for (int i = tamanhoPilha-1; i >= 0 ; i--) {
+            System.out.print(auxiliar[i]);
+        }
+
+        System.out.print("\n");
     }
 }
