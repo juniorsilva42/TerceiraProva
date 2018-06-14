f6f2acc3a42e7cbcc45da8fc8af77f6d3107ba57
ivaniciojr <jsiilva@outlook.com.br>
2018-06-14 18:05:12 -0300

    Documentando a classe da Fila

--- src/br/com/ivaniciojr/filas/Fila.java
+++ src/br/com/ivaniciojr/filas/Fila.java
@@ -8,15 +8,31 @@ public class Fila {
     private List<Object> fila;
     private int capacidade = 100;
 
+    /*
+     *
+     * Aloca a ArrayList com uma capacidade predefinida de 100 elementos
+     *
+     * */
     public Fila () {
         fila = new ArrayList<Object>(capacidade);
     }
 
+    /*
+     *
+     * Procedimento para enfileirar um item na fila, adiciona o elemento no fim da ArrayList
+     *
+     * */
     public void enfileira (Object elemento) {
         this.fila.add(elemento);
     }
 
+    /*
+     *
+     * Procedimento para desenfileira um item da fila, removendo sempre o elemento cabeça
+     *
+     * */
     public Object desenfileira () {
+        // Verifica se a fila não está vazia, o contrário disso não faz sentido tentar remover
         if (!this.estaVazia()) {
             return this.fila.remove(0);
         } else {
@@ -24,10 +40,23 @@ public class Fila {
         }
     }
 
+    /*
+     *
+     * Procedimento para furar a fila, ou seja, inserir um elemento na lista numa dada posição.
+     *
+     * */
     public void furaFila (Object valor, int posicao) {
 
+        // Se a fila não estiver cheia, ou seja, não ultrapassar os 100 elementos
         if (!this.estaCheia()) {
+            // Verifica se existe a posição
             if (verificaPosicao(posicao)) {
+                /*
+                *
+                * Adiciono o elemento corrente na posição informada no argumento da função na próxima posição,
+                * em seguida, deleto o mesmo, e adiciona naquela posição o novo elemento.
+                *
+                * */
                 this.fila.add(posicao+1, this.fila.get(posicao));
                 fila.remove(posicao);
                 this.fila.add(posicao, valor);
@@ -37,14 +66,29 @@ public class Fila {
         }
     }
 
+    /*
+     *
+     * Método para verificar se a fila está vazia
+     *
+     * */
     public boolean estaVazia () {
         return this.fila.size() == 0;
     }
 
+    /*
+     *
+     * Método para verificar se a fila está cheia
+     *
+     * */
     public boolean estaCheia () {
-        return this.fila.size() == 100;
+        return this.fila.size() == capacidade;
     }
 
+    /*
+     *
+     * Método para verificar a existência de uma dada posição
+     *
+     * */
     public boolean verificaPosicao (int posicao) {
         if (!(posicao >= 0 && posicao < this.fila.size())) {
             return false;
@@ -52,6 +96,12 @@ public class Fila {
         return true;
     }
 
+    /*
+     *
+     * Método para montar os elementos da fila numa String
+     *
+     * */
+    @Override
     public String toString () {
 
         StringBuilder s = new StringBuilder();
